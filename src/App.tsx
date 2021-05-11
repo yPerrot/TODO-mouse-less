00eba84d6a9b649936413ff28a1d8be4e46d9ec9
unknown <y.perrot@outlook.fr>
2021-05-11 14:32:14 +0200

    Add sort task feature

--- src/App.tsx
+++ src/App.tsx
@@ -12,24 +12,40 @@ const appStyle: CSS.Properties = {
 function App() {
 
     const [ tasks, setTasks] = useState<string[]>(['task1','task2']);
+    const [ isAscendingOrder, setIsAscendingOrder] = useState(true);
 
-    const addTask = (newTask:string) => {
-        setTasks((oldArray) => oldArray.concat(newTask))
-        console.log(newTask);
+    const addTask = (newTask:string, order:boolean) => {
+        if (order) {
+            setTasks((oldArray) => oldArray.concat(newTask))
+        } else {
+            setTasks((oldArray) => [newTask].concat(oldArray))
+        }
     }
 
     const deleteTask = (removedTask:string) => {
-        setTasks((oldArray) => oldArray.filter(task => task !== removedTask))
+        setTasks((oldArray) => oldArray.filter(task => task !== removedTask));
         console.log(removedTask);
+    }
+
+    const changeOrder = () => {
+        setIsAscendingOrder(!isAscendingOrder);
+        setTasks((oldArray) => [...oldArray].reverse());
     }
 
     return (
         <div style={appStyle}>
             <table><tbody>
+            <tr><td>
+                <button onClick={changeOrder}>Change order</button>
+            </td></tr>
+            <tr><td>
+                <NewTask isAscendingOrder={isAscendingOrder} addTask={addTask}/>
+            </td></tr>
             {tasks.map((e, id) => (
-                <tr key={id}><td><Task text={e} type="" deleteTask={deleteTask}/></td></tr>
+                <tr key={id}><td>
+                    <Task text={e} type="" deleteTask={deleteTask}/>
+                </td></tr>
             ))}
-            <tr><td><NewTask addTask={addTask}/></td></tr>
             </tbody></table>
         </div>
     );
